65dc22b5439a2e1e89de4cf487b420467ba55522
stop2stare <emitremmus0235@163.com>
2017-01-17 14:22:34 +0800

    init commit

---
+++ app/electron.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const electron = require('electron')
+const path = require('path')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const menubar = require('menubar');
+
+let config = {}
+
+if (process.env.NODE_ENV === 'development') {
+  config = require('../config')
+  config.url = `http://localhost:${config.port}`
+} else {
+  config.devtron = false
+  config.url = `file://${__dirname}/dist/index.html`
+}
+
+let mb = new menubar({
+	index: config.url
+})
+
+
+mb.on('ready', () => {
+  console.log('Hello, it\'s Moki!')
+})
+
+// function createWindow () {
+//   /**
+//    * Initial window options
+//    */
+//   mainWindow = new BrowserWindow({
+//     height: 600,
+//     width: 800
+//   })
+
+//   mainWindow.loadURL(config.url)
+
+//   if (process.env.NODE_ENV === 'development') {
+//     BrowserWindow.addDevToolsExtension(path.join(__dirname, '../node_modules/devtron'))
+
+//     let installExtension = require('electron-devtools-installer')
+
+//     installExtension.default(installExtension.VUEJS_DEVTOOLS)
+//       .then((name) => mainWindow.webContents.openDevTools())
+//       .catch((err) => console.log('An error occurred: ', err))
+//   }
+
+//   mainWindow.on('closed', () => {
+//     mainWindow = null
+//   })
+
+//   console.log('mainWindow opened')
+// }
+
+// app.on('ready', createWindow)
+
+// app.on('window-all-closed', () => {
+//   if (process.platform !== 'darwin') {
+//     app.quit()
+//   }
+// })
+
+// app.on('activate', () => {
+//   if (mainWindow === null) {
+//     createWindow()
+//   }
+// })
---
+++ app/src/components/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>It's Moki!</div>
+</template>
+<script>
+export default {}
+</script>
