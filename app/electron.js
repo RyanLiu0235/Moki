a4f3ec0e39f9ff148dd9eee0f5d119d6a4de6a57
stop2stare <emitremmus0235@163.com>
2017-01-18 20:05:45 +0800

    create browser window

--- app/electron.js
+++ app/electron.js
@@ -18,11 +18,9 @@ let image = nativeImage.createFromPath(`${__dirname}/icons/moki.png`)
 
 app.on('ready', () => {
   tray = new Tray(image)
-  const contextMenu = Menu.buildFromTemplate([
-    {label: 'Item1', type: 'radio'},
-    {label: 'Item2', type: 'radio'},
-    {label: 'Item3', type: 'radio', checked: true},
-    {label: 'Item4', type: 'radio'}
-  ])
-  tray.setContextMenu(contextMenu)
+  const win = new BrowserWindow({ width: 800, height: 600 })
+  win.loadURL(config.url)
+  tray.on('click', () => {
+    win.isVisible() ? win.hide() : win.show()
+  })
 })
