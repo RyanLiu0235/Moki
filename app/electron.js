1d69840e599a60035601ad11837d5b3049d419d5
stop2stare <emitremmus0235@163.com>
2017-01-18 19:53:37 +0800

    add icons

--- app/electron.js
+++ app/electron.js
@@ -1,13 +1,9 @@
 'use strict'
 
-const electron = require('electron')
+const { app, Menu, BrowserWindow, Tray, nativeImage } = require('electron')
 const path = require('path')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
-const menubar = require('menubar');
 
 let config = {}
-let mainWindow
 
 if (process.env.NODE_ENV === 'development') {
   config = require('../config')
@@ -17,53 +13,16 @@ if (process.env.NODE_ENV === 'development') {
   config.url = `file://${__dirname}/dist/index.html`
 }
 
-let mb = new menubar({
-	index: config.url
-})
+let tray = null
+let image = nativeImage.createFromPath(`${__dirname}/icons/moki.png`)
 
-
-mb.on('ready', () => {
-  console.log('Hello, it\'s Moki!')
-})
-
-function createWindow () {
-  /**
-   * Initial window options
-   */
-  mainWindow = new BrowserWindow({
-    height: 600,
-    width: 800
-  })
-
-  mainWindow.loadURL(config.url)
-
-  if (process.env.NODE_ENV === 'development') {
-    BrowserWindow.addDevToolsExtension(path.join(__dirname, '../node_modules/devtron'))
-
-    let installExtension = require('electron-devtools-installer')
-
-    installExtension.default(installExtension.VUEJS_DEVTOOLS)
-      .then((name) => mainWindow.webContents.openDevTools())
-      .catch((err) => console.log('An error occurred: ', err))
-  }
-
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
-
-  console.log('mainWindow opened')
-}
-
-app.on('ready', createWindow)
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow()
-  }
+app.on('ready', () => {
+  tray = new Tray(image)
+  const contextMenu = Menu.buildFromTemplate([
+    {label: 'Item1', type: 'radio'},
+    {label: 'Item2', type: 'radio'},
+    {label: 'Item3', type: 'radio', checked: true},
+    {label: 'Item4', type: 'radio'}
+  ])
+  tray.setContextMenu(contextMenu)
 })
