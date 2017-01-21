34227b1c28354ec8ef10cc6542f8d97c22b38004
stop2stare <emitremmus0235@163.com>
2017-01-21 18:46:07 +0800

    generate menu items automatically

--- app/controller/menu.controller.js
+++ app/controller/menu.controller.js
@@ -1,12 +1,66 @@
-exports.citiesMenuGenerater = function (citiesArray, handleClick) {
+const separator = [{ type: 'separator' }]
+const winMenu = [
+  { label: '最小化', type: 'normal', role: 'minimize' },
+  { label: '退出', type: 'normal', role: 'quit' }
+]
+let chooseCityMenu = function(goTo) {
+  return [{
+    label: '更多',
+    type: 'normal',
+    click: function() {
+    	goTo('setting-page')
+    }
+  }]
+}
+let infoMenu = [{ label: '暂无数据', type: 'normal' }]
+let settingMenu = [{
+  label: '选择城市',
+  type: 'submenu',
+  submenu: []
+}, {
+	label: '刷新',
+	type: 'normal',
+	role: 'reload'
+},{
+  label: '设置',
+  type: 'normal'
+}]
+
+const citiesMenuGenerater = function(citiesArray, handleClick) {
   return citiesArray.map((city, index) => {
     return {
       label: city.name,
       type: 'radio',
       checked: city.meta.checked,
-      click: function(menuItem, browserWindow, event) {
+      click: function() {
         handleClick(city.name)
       }
     }
   })
+}
+
+/**
+ * generate menu template for menu
+ * 
+ * @param  {Object} win         
+ * @param  {Array} citiesArray 
+ * @return {Array} 
+ */
+exports.menuTemplateGenerator = function(win, citiesArray) {
+  function changeCity(city) {
+    win.webContents.send('change-city', city)
+  }
+
+  function goTo(page) {
+    win.webContents.send('go-to', page)
+  }
+  settingMenu[0]['submenu'] = citiesMenuGenerater(citiesArray, changeCity)
+    .concat(separator)
+    .concat(chooseCityMenu(goTo))
+
+  return infoMenu
+    .concat(separator)
+    .concat(settingMenu)
+    .concat(separator)
+    .concat(winMenu)
 }
--- app/electron.js
+++ app/electron.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { app, Menu, BrowserWindow, Tray, nativeImage, ipcMain } = require('electron')
-const { citiesMenuGenerater } = require('./controller/menu.controller')
+const { menuTemplateGenerator } = require('./controller/menu.controller')
 
 let config = {}
 
@@ -18,76 +18,20 @@ let image = nativeImage.createFromPath(`${__dirname}/icons/moki.png`)
 
 // app
 app.on('ready', () => {
-  let citiesArray = []
-  let menuTemplate = []
-  let contextMenu
-    // create window 
+  // create window 
   const win = new BrowserWindow({ width: 1000, height: 600 })
   win.loadURL(config.url)
   win.webContents.openDevTools()
 
-  let chooseCityMenu = [{
-    label: '更多',
-    type: 'normal',
-    click: function() {
-      win.webContents.send('go-to', 'city-page')
-    }
-  }]
-
-  let separator = [{ type: 'separator' }]
-  let infoMenu = [{ label: '暂无数据', type: 'normal' }]
-  let settingMenu = [{
-    label: '选择城市',
-    type: 'submenu',
-    submenu: []
-  }, {
-    label: '打开窗口',
-    type: 'normal',
-    click: function(menuItem, browserWindow) {
-      win.show()
-      win.webContents.openDevTools()
-    }
-  }, {
-    label: '设置',
-    type: 'normal',
-    click: function(menuItem, browserWindow) {
-      win.webContents.send('change-city', 'setting-page');
-    }
-  }]
-  let winMenu = [
-    { label: '最小化', type: 'normal', role: 'minimize' },
-    { label: '退出', type: 'normal', role: 'quit' }
-  ]
-
   // create tray
   tray = new Tray(image)
 
-  settingMenu[0]['submenu'] = citiesMenuGenerater(citiesArray, function(city) {
-      win.webContents.send('change-city', city)
-    })
-    .concat(separator)
-    .concat(chooseCityMenu)
-
-  menuTemplate = infoMenu
-    .concat(separator)
-    .concat(settingMenu)
-    .concat(separator)
-    .concat(winMenu)
-  contextMenu = Menu.buildFromTemplate(menuTemplate)
+  let menuTemplate = menuTemplateGenerator(win, [])
+  let contextMenu = Menu.buildFromTemplate(menuTemplate)
   tray.setContextMenu(contextMenu)
 
   ipcMain.on('update-city', (event, cities) => {
-    settingMenu[0]['submenu'] = citiesMenuGenerater(cities, function(city) {
-        win.webContents.send('change-city', city)
-      })
-      .concat(separator)
-      .concat(chooseCityMenu)
-
-    menuTemplate = infoMenu
-      .concat(separator)
-      .concat(settingMenu)
-      .concat(separator)
-      .concat(winMenu)
+    menuTemplate = menuTemplateGenerator(win, cities)
     contextMenu = Menu.buildFromTemplate(menuTemplate)
     tray.setContextMenu(contextMenu)
   })
